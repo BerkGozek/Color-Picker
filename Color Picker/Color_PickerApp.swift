2364ee048a7c70407648981149ec0905e6510ae4
Berk GOZEK <81320354+BerkGozek@users.noreply.github.com>
2023-09-09 21:36:32 +0300

    Initial Commit

---
+++ Color Picker/Color_PickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Color_PickerApp.swift
+//  Color Picker
+//
+//  Created by Berk Gozek on 09/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Color_PickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
